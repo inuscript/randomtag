f4ac3b1990c9a4587715b4d3ad2e00299da85fcc
inuscript <inuscript@gmail.com>
2015-12-29 00:59:44 +0900

    fix random

--- random.js
+++ random.js
@@ -1,14 +1,10 @@
 (function(){
-  var masterTags = [  "dog",
-    "norfolkterrier",
-  ];
-  var randomTags = [
-    "instadog", "dogs", "doglover", "doggie", "dogstagram", "smalldog",
-    "dogslife", "adorable", "pets", "dogsofinstagram", "instaterrier",
-    "cutedog", "dogoftheday", "animal", "petstagram", "blackandtan",
-    "petsofinstagram", "hound", "happy_pet", "terrier", "dailydog",
-    "bestwoof", "cute",
-  ];
+  var masterTags = ["dog", "norfolkterrier"]
+  var randomTags = ["adorable", "animal", "animales", "animallovers", "animals", "bestwoof", "blackandtan", "cute", "cutedog", "cutie", "dailydog", "dog", "doggie", "doggy", "doglover", "doglovers", "dogoftheday", "dogs", "dogs_of_instagram", 
+  "dogsitting", "dogslife", "dogsofinstagram", "dogstagram", 
+  "happy_pet", "hound", "ilovedog", "ilovedogs", "ilovemydog", "instadog", "instagood", "instagramdogs", "instapuppy", "instaterrier", "life", "love", "lovedogs", 
+  "lovepuppies", "nature", "pet", "pets", "pets_of_instagram", "petsagram", "petsofinstagram", "petstagram", "photooftheday", "picpets", "precious", "pup", "puppies", "puppy", "smalldog", "tagblender", "terrier", "terriers", "terrierstagram"]
+  // randomTags.sort().filter((x, i, self) => { return self.indexOf(x) === i })
   var shuffle = function(arr) {
     var random = arr.map(Math.random);
     arr.sort(function(a, b) {
@@ -18,7 +14,9 @@
   }
   var getSuffled = function(){
     var tags = masterTags.concat()
-    var rand = shuffle(randomTags).slice(0, randomTags.length * 0.7)
+    var maxRandTag =  30 - masterTags.length
+    var len = Math.min(randomTags.length * 0.7, maxRandTag)
+    var rand = shuffle(randomTags).slice(0, len)
     return shuffle(tags.concat(rand))
   }
   var arrToTagStr = function(arr){
