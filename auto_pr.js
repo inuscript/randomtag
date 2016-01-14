062efcdde3a39156d0dcc2321ee125774050cd64
inuscript <inuscript@gmail.com>
2016-01-14 09:36:17 +0900

    wip

--- auto_pr.js
+++ auto_pr.js
@@ -5,8 +5,8 @@ const CIRCLE_BUILD_NUM = process.env.CIRCLE_BUILD_NUM
 const CIRCLE_ARTIFACTS = process.env.CIRCLE_ARTIFACTS 
 
 console.log(GITHUB_ACCESS_TOKEN, CIRCLE_PROJECT_USERNAME, CIRCLE_PROJECT_REPONAME , CIRCLE_BUILD_NUM, CIRCLE_ARTIFACTS)
-
-const url=`https://circle-artifacts.com/gh/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts/0${CIRCLE_ARTIFACTS}/tags.txt`
+const BASE_URL = "https://circle-artifacts.com"
+const url=`${BASE_URL}/gh/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts/0${CIRCLE_ARTIFACTS}/tags.txt`
 console.log(url)
 
 // https://circle-artifacts.com/gh/inuscript/randomtag/22/artifacts/0/home/ubuntu/randomtag/tags.txt
