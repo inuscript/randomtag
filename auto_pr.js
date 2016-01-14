3a73598eb63fb73ff0c43f85e1e9d65067a6c598
inuscript <inuscript@gmail.com>
2016-01-14 21:36:38 +0900

    remove

--- auto_pr.js
+++ auto_pr.js
@@ -2,7 +2,5 @@ const fs = require("fs")
 const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN
 const CIRCLE_ARTIFACTS = process.env.CIRCLE_ARTIFACTS 
 
-console.log(url)
-
 const items = fs.readFileSync(`${CIRCLE_ARTIFACTS}/tags.txt`)
 console.log(items)
