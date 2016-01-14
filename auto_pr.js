b2b03ecd66f1566d5f32b430fac98de95684bb7c
inuscript <inuscript@gmail.com>
2016-01-14 22:27:29 +0900

    add

--- auto_pr.js
+++ auto_pr.js
@@ -1,6 +1,20 @@
 const fs = require("fs")
+const github = require("github-basic")
 const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN
 const CIRCLE_ARTIFACTS = process.env.CIRCLE_ARTIFACTS 
 
-const items = fs.readFileSync(`${CIRCLE_ARTIFACTS}/tags.txt`)
-console.log(items)
+const file = fs.readFileSync(`${CIRCLE_ARTIFACTS}/tags.txt`, 'utf-8')
+var client = github({version: 3, 
+  auth: GITHUB_ACCESS_TOKEN
+})
+var user = "inuscript"
+var repo = "randomtag"
+
+client.exists(user, repo, (err, res) => {
+  console.log(err, res)
+})
+
+client.get(`/repos/${user}/${repo}/issues`, {}, (err, res) => {
+  if (err) throw err;
+  console.dir(res)
+})
