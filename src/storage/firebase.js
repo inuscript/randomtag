a7cf2b65380bf16ed442b70aa9419b6ce233cf0e
inuscript <inuscript@gmail.com>
2016-02-10 23:02:03 +0900

    reject -1 day data

--- src/storage/firebase.js
+++ src/storage/firebase.js
@@ -7,9 +7,12 @@ export default class {
   media(){ 
     // after 2015-12-28
     let start = new Date(2015, 11, 28).getTime()
+    let end = new Date().getTime() - 1000 * 60 * 60 * 24 // - 1 day
     let mediaRef = this.ref
       .child("media")
-      .orderByChild("time").startAt(start)
+      .orderByChild("time")
+      .startAt(start)
+      .endAt(end)
     return new Promise( (resolve, reject) => {
       mediaRef.once("value", (snap) => {
         let items = Object.values(snap.val())
