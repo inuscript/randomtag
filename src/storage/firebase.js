894ab9bffc6fc3d3f81063ee5378693109cb56e5
inuscript <inuscript@gmail.com>
2016-02-10 22:26:21 +0900

    fix comment

--- src/storage/firebase.js
+++ src/storage/firebase.js
@@ -4,8 +4,9 @@ export default class {
   constructor(){
     this.ref = new Firebase("http://thridsta.firebaseio.com")
   }
-  media(){ // TODO: after 2015-12-28
-    let start = new Date(2015, 11, 28).getTime() // 2015-12-28
+  media(){ 
+    // after 2015-12-28
+    let start = new Date(2015, 11, 28).getTime()
     let mediaRef = this.ref
       .child("media")
       .orderByChild("time").startAt(start)
