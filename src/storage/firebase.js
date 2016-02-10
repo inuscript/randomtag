f3283fd58f42971eb3363a58898c0c1e06dbb560
inuscript <inuscript@gmail.com>
2016-02-10 22:26:01 +0900

    use startAt()

--- src/storage/firebase.js
+++ src/storage/firebase.js
@@ -4,8 +4,11 @@ export default class {
   constructor(){
     this.ref = new Firebase("http://thridsta.firebaseio.com")
   }
-  media(num = 30){ // TODO: after 2015-12-28
-    let mediaRef = this.ref.child("media").orderByChild("time").limitToLast(num)
+  media(){ // TODO: after 2015-12-28
+    let start = new Date(2015, 11, 28).getTime() // 2015-12-28
+    let mediaRef = this.ref
+      .child("media")
+      .orderByChild("time").startAt(start)
     return new Promise( (resolve, reject) => {
       mediaRef.once("value", (snap) => {
         let items = Object.values(snap.val())
