1fb7a6dcfabac1ee31d83074f94eba87f556487e
inuscript <inuscript@gmail.com>
2016-03-10 21:58:21 +0900

    add

--- src/bandit/calc.js
+++ src/bandit/calc.js
@@ -4,7 +4,7 @@ export default function bandit(tags, normalize){
   let bandit = new UCBBandit( tags )
   normalize.forEach( ({tag, count}) => {
     count.forEach( c => {
-      for(let i = 0; i < 30 i++){
+      for(let i = 0; i < 20; i++){
         bandit.reward(tag, c)
       }
     })
