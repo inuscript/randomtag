fe83df1b4f6401b75558867daef62582ae32f616
inuscript <inuscript@gmail.com>
2016-03-27 01:20:19 +0900

    start point

--- src/index.js
+++ src/index.js
@@ -10,16 +10,19 @@ import Cycle from '@cycle/core';
 import { makeDOMDriver} from '@cycle/dom';
 import { tags, stats } from "./mock"
 
-docReady( function(){
+function startWithServer(){
+  let ts = calcTags().then( ({tags, stats}) => {
+    main({tags, stats})
+  }).catch(e => {
+    console.error(e)
+  })
+}
+function startWithMock(){
   main({tags, stats})
-
+}
+docReady( function(){
+  // main({tags, stats})
+  // startWithServer()
+  startWithMock()
   // let container = document.getElementById('container')
-  // let ts = calcTags().then( ({tags, stats}) => {
-  //   console.log(JSON.stringify(tags, null, 2), JSON.stringify(stats, null, 2))
-  //   container.innerHTML = "" // clean
-  //   // mountToDom(container, node(App).attrs({tags, stats}));
-  //   Cycle.run(main, {DOM: makeDOMDriver('#container')})
-  // }).catch(e => {
-  //   console.error(e)
-  // })
 })
