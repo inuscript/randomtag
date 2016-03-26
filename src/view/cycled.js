dbb9c82469b364e2bdec4a07c218fd7460985a80
inuscript <inuscript@gmail.com>
2016-03-27 01:54:30 +0900

    component?

--- src/view/cycled.js
+++ src/view/cycled.js
@@ -2,34 +2,42 @@
 import Rx, {Observable} from 'rx';
 import Cycle from '@cycle/core';
 import {makeDOMDriver, hJSX, h} from '@cycle/dom';
+import Clipboard from "clipboard"
 
-function tagComponent(tag){
-  return <span className="tag-item">{`#${tag} `}</span>
+function Hashtag({DOM, tag}){
+  // const intent = DOM.select('.tag-item').events('click').map( )
+  return {
+    DOM: <span className="tag-item">{`#${tag} `}</span>
+  }
 }
-function tagsComponent(tags){
-  return <div>{ tags.map( tag => tagComponent(tag) ) }</div>
+function HashTags({DOM, tags}){
+  return {
+    DOM: <div>{ tags.map( tag => Hashtag({DOM, tag}).DOM ) }</div>
+  }
+}
+
+function CopyButton({DOM, target}){
+  let id = "__copy__button__"
+  let clipboard = new Clipboard(`#${id}`)
+  return {
+    DOM: <button id={id} data-clipboard-target={target} >Copy !</button>
+  }
 }
 
 export function app({DOM, props}){
   // let state$ = Rx.Observable.from(props.tags) //, (tag) => li([tag]) ) //.mergeAll()
   let props$ = Observable.just(props)
   // console.log(props$.map)
+  let copyButton = CopyButton({DOM, target: "foo"})
   return {
     DOM: props$.map( ({tags, stats}) => {
-      let tagsView = tagsComponent(tags)
+      let hashTags = HashTags({DOM, tags: tags})
       return <div>
         <h1>Randomtag</h1>
-        <div>{ tagsView }</div>
+        { copyButton.DOM }
+        { hashTags.DOM }
       </div>
     } )
-    // tags$.map( (items) =>{
-    //   console.log("====")
-    //   console.log(items)
-    //   return div(items)
-    // })
-    // .withLatestFrom(tags$, (tags, v) => {
-    //   return div([tags])
-    // })
   }
 }
 
