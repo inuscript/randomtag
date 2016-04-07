1f262731863bb4e07346d9c59094c2f17c9724e4
inuscript <inuscript@gmail.com>
2016-04-08 00:22:03 +0900

    use custom eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,5 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-    },
     "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        },
-    },
-    "plugins": [
-        "standard",
-        "react"
-    ],
-    "rules": {
-        "semi": ["error", "never"],
-        "react/prop-types": ["off"],
-        "react/jsx-uses-vars": 1,
-        // "arrow-parens": ["off"],
-        // "space-before-blocks": ["off"],
-        // "space-before-function-paren": ["off"],
-    }
+        "@inuscript/eslint-config-inuscript"
+    ]
 };
