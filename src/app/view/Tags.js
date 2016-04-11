6b30984c9f350b51fdafb06b90697abdd7c8589c
inuscript <inuscript@gmail.com>
2016-04-12 00:44:34 +0900

    function tagWrap

--- src/app/view/Tags.js
+++ src/app/view/Tags.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { withProps, renderComponent, compose } from 'recompose'
+import { withProps, compose, branch, renderNothing } from 'recompose'
+
 const Tag = ({tagLabel, onClick}) => {
   // なぜか最後に空白が無いとcssが崩れる。謎。
   let {label, num, exp} = tagLabel
@@ -10,17 +11,25 @@ const Tag = ({tagLabel, onClick}) => {
   </span>
 }
 
+const tagWrap = (tag, onTagClick) => (
+  branch(
+    () => tag.selected, 
+    withProps({
+      tagLabel:tag, 
+      onClick: () => onTagClick(tag.label)
+    }),
+    renderNothing()
+  )
+)(Tag)
+
+
 export default class Tags extends Component {
   render () {
     let { onTagClick, hashTags } = this.props
     return <div>{
       hashTags.map((t,i) => {
-        if(t.selected){
-          let props = {tagLabel:t, onClick: () => onTagClick(t.label)}
-          let TagItem = withProps(props)(Tag)
-          return <TagItem key={i} />
-        }
-        return null
+        let TagItem = tagWrap(t, onTagClick)
+        return <TagItem key={i} />
       })
     }</div>
   }
