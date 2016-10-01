f4a6e7150a9cc3e94121973e32fde2e1652c9e41
inuscript <inuscript@gmail.com>
2016-10-01 10:34:51 +0900

    add <Copy> story

--- src/app/view/stories/index.js
+++ src/app/view/stories/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Button from './Button';
-import Welcome from './Welcome';
+import Copy from '../Copy';
 
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
-  ));
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+storiesOf('Randomtag', module)
+  .add('Copy', () => (
+    <Copy onClick={action('onclick')} tags={["foo", "baz", "bar"]}/>
   ));
