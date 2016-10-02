5a8a11b7d4dbed3dc5bc1b1d76989f811400e5e4
inuscript <inuscript@gmail.com>
2016-10-02 15:00:16 +0900

    wip

---
+++ src/app/view/badCopy.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Clipboard from 'clipboard'
+
+export default class Copy extends Component {
+  get copyString(){
+    const { tags } = this.props
+    return tags.map((tag) => `#${tag}`).join(' ')
+  }
+
+  render(){
+    const buttonId = '__copy__button__'
+    const targetId = '__copy__button__target__'
+    this.clipboard = new Clipboard(`#${buttonId}`)
+    this.clipboard.on('success', () => {
+      // TODO
+    })
+    return <div>
+      <button id={buttonId} data-clipboard-target={`#${targetId}`} >Copy !</button>
+      <div id={targetId} className='copy-tag'>{this.copyString}</div>
+    </div>
+  }
+}
